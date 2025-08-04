38cd917a3468636d96d31a0221e5b2baf3452d05
Ali Raza <araza@insurquote.org>
2025-08-05 02:09:13 +0500

    Improve UI

--- script.js
+++ script.js
@@ -5,30 +5,3 @@ AOS.init({
 });
 
 
-const toggle = document.getElementById('modeToggle');
-const body = document.body;
-const navbar = document.querySelector('.navbar');
-
-function setMode(mode) {
-  if (mode === 'light') {
-    body.classList.add('light-mode');
-    navbar.classList.remove('bg-dark');
-    navbar.classList.add('bg-light', 'navbar-light');
-    toggle.checked = true;
-  } else {
-    body.classList.remove('light-mode');
-    navbar.classList.add('bg-dark');
-    navbar.classList.remove('bg-light', 'navbar-light');
-    toggle.checked = false;
-  }
-}
-
-setMode('dark');
-
-toggle.addEventListener('change', () => {
-  if (toggle.checked) {
-    setMode('light');
-  } else {
-    setMode('dark');
-  }
-});
